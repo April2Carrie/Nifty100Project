1581893a1351e4f9962490dd980ac0246808e3c1
April2Carrie <yinglandeng2001@gmail.com>
2022-12-08 23:54:38 -0600

    Update final code.

--- code/arima.R
+++ code/arima.R
@@ -2,15 +2,31 @@
 require("zoo")
 require("forecast")
 
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 dat <- read.csv(args[1])
-
+origin<-read.zoo(dat)
+n.train <- floor(0.8*length(dat[,1]))
+n.test <- length(dat[,1])-n.train
 # build model
-dat <- read.zoo(dat)
-m <- auto.arima(dat)
+train <- read.zoo(dat[1:n.train,])
+test <- read.zoo(dat[(n.train+1):(n.train+n.test),])
 
-pdf(paste(args[1],".pdf",sep=""))
-plot(forecast(m,h=100))
+m <- auto.arima(train)
+name <- sub(".csv","",args[1])
+
+
+pdf(paste(name,"_predict.pdf",sep=""))
+plot(forecast(m,h=n.test),xaxt='n',xlab='time',ylab='price')
+lines(test)
+legend("topleft",legend=c("real","predict"),lty = c(1, 1), col = c("black", "#6699CC"), lwd = 2)
+dev.off()
+
+pdf(paste(name,"_check.pdf",sep=""))
+checkresiduals(m)
+dev.off()
+
+pdf(paste(name,"_root.pdf",sep=""))
+autoplot(m)
 dev.off()
 
 print(summary(m))
